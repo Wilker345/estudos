4c2a5d871084fe89217e68f1ded3a62bb667d780
Carlos Wilker <programmerwilker@gmail.com>
2021-07-28 17:41:45 -0300

    via VScode

---
+++ JS/JSlogicaDeProgramacao/switchCase.js
@@ -0,0 +1,9 @@
+//checa o valor da variável test para decidir o valor da variável resultado
+switch(test){
+    case 0:
+        resultado = 'F'
+        break
+    case 1:
+        resultado = 'V'
+        break
+}
