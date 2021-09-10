ca5ff96532a4d3cc7df4882e5b915800bf6953e4
Carlos Wilker <programmerwilker@gmail.com>
2021-09-10 19:50:21 -0300

    via VScode

---
+++ praticaTypescript/dist/2-tipoAny.js
@@ -0,0 +1,7 @@
+"use strict";
+function showMessage(msg) {
+    return msg;
+}
+//tipo "qualquer"
+console.log(showMessage([1, 4, 2]));
+console.log(showMessage('a'));
---
+++ praticaTypescript/src/2-tipoAny.ts
@@ -0,0 +1,6 @@
+function showMessage(msg: any) {
+  return msg;
+}
+//tipo "qualquer"
+console.log(showMessage([1, 4, 2]));
+console.log(showMessage('a'));
