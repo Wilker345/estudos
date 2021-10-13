55c87dab1986a22acb5ebbd5d53357264d8b789d
Carlos Wilker <programmerwilker@gmail.com>
2021-10-13 18:42:54 -0300

    via VSCode

---
+++ tela-login-final/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class telaAbsoluta extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = '';
+  }
+}
+
+class receberEmail extends React.Component{
+}
+
+class receberSenha extends React.Component{
+}
+
+class botaoLogin extends React.Component{
+}
+
+class botaoGoogle extends React.Component{
+}
+
+
+
+ReactDOM.render(<botaoGoogle/>,
+  document.getElementById('root')
+);
+
