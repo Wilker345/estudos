f6d3063e0605d6b0d6eae76e3e9d183fe4fb5c99
Carlos Wilker <programmerwilker@gmail.com>
2021-10-18 14:03:43 -0300

    via VSCode

--- nlw/letmeask/src/App.tsx
+++ nlw/letmeask/src/App.tsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import { Home } from "./pages/home";
-
 import { NewRoom } from "./pages/NewRoom";
 
+import { AuthContextProvider } from './contexts/AuthContext'
+
 function App() {
+
+
   return (
     <BrowserRouter>
-      <Route path="/" exact={true} component={Home}/>
-      <Route path="/rooms/new" component={NewRoom} />
+      <AuthContextProvider>
+        <Route path="/" exact={true} component={Home}/>
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
