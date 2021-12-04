3f5a2cee26499f7d4e0ba6ce59449e9cf0fa3352
Carlos Wilker <programmerwilker@gmail.com>
2021-12-04 15:05:58 -0300

    add usehistory

--- crud/client/src/pages/login.js
+++ crud/client/src/pages/login.js
@@ -1,13 +1,18 @@
+import { useHistory } from 'react-router';
 import React from "react";
 export function Login (){
+  const history = useHistory();
+  function aoCrud() {
+    history.push('/crud');
+  };
   return(
     <div className="telaLogin">
       <input type='text' placeholder='Nome'/>
       <br/>
       <input type ='text' placeholder='Senha'/>
       <br/>
-      <button className='botaoLogin'>Login</button>
-      <button className='botaoGoogle'>Entrar com Google</button>
+      <button className='botaoLogin' onClick={aoCrud}>Login</button>
+      <button className='botaoGoogle' onClick={aoCrud}>Entrar com Google</button>
 
     </div>
   )
