adb2771e5b482d9ee54fdbff52aa7732e3e07b3c
Carlos Wilker <programmerwilker@gmail.com>
2021-12-15 20:39:27 -0300

    implementacao mui e mudancas visuais na pg login

--- crud/client/src/pages/login.js
+++ crud/client/src/pages/login.js
@@ -1,20 +1,97 @@
 import { useHistory } from 'react-router';
 import React from "react";
+import { useState } from 'react';
+import {Typography, Container, Button, TextField} from '@mui/material';
+import GoogleIcon from '@mui/icons-material/Google';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 export function Login (){
+  const[email, setEmail] = useState('')
+  const[pass, setPass] = useState('')
+
+  const[emailError, setEmailError] = useState(false)
+  const[passError, setPassError] = useState(false)
+
   const history = useHistory();
   function aoCrud() {
     history.push('/crud');
   };
-  return(
-    <div className="telaLogin">
-      <input type='text' placeholder='Nome'/>
-      <br/>
-      <input type ='text' placeholder='Senha'/>
-      <br/>
-      <button className='botaoLogin' onClick={aoCrud}>Login</button>
-      <button className='botaoGoogle' onClick={aoCrud}>Entrar com Google</button>
 
-    </div>
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    setEmailError(false)
+    setPassError(false)
+
+    if ( email === ''){
+      setEmailError(true)
+    }
+    if ( pass === ''){
+      setPassError(true)
+    }
+
+    if (email && pass) {
+      console.log(email, pass)
+
+    }
+  }
+
+  return(
+    <Container>
+    <form noValidate autoComplete="off" onSubmit={handleSubmit} className='telaLogin'>
+    <Typography
+    component="h1"
+    variant="h5"
+    >Tela de Login
+    </Typography>
+      <TextField
+      sx={{
+        marginTop: 2,
+        marginBottom: 2,
+        display: 'block'
+        }}
+      fullWidth
+      label="Email"
+      type='text'
+      variant="outlined"
+      required
+      error={emailError}
+      />
+      <TextField
+      sx={{
+        marginBottom: 2,
+        display: 'block'
+        }}
+      fullWidth
+      label="Senha"
+      type='text'
+      variant="outlined"
+      required
+      error={passError}
+      />
+      <Button
+      sx={{
+        marginBottom: 1,
+        }}
+      variant='contained'
+      className='botaoLogin'
+      onClick={aoCrud}
+      >Login
+      </Button>
+
+      <Button
+      variant='contained'
+      size='large'
+      color='error'
+      startIcon={<GoogleIcon/>}
+      onClick={aoCrud}
+      >Entrar com Google
+      </Button>
+      </form>
+      </Container>
   )
 }
 
