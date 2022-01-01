59a8c318b42db20c1b203fc83886a1c1daa0abaa
Carlos Wilker <cwilkersantana@gmail.com>
2022-01-01 17:23:21 -0300

    comeco datagrid

--- crud/client/src/App.js
+++ crud/client/src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Crud } from './pages/crud';
 import { Login } from './pages/login';
-
+import { Grid } from './pages/datagrid'
 function App() {
 
   return (
   <BrowserRouter>
       <Route exact path="/"  component={Login} />
       <Route path="/crud" component={Crud} />
+      <Route path="/grid" component={Grid} />
   </BrowserRouter>
   );
 }
